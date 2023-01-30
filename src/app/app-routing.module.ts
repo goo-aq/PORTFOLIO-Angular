c39b3860bfb77dda020d4622987a71cbe941e491
goo-aq <120787130+goo-aq@users.noreply.github.com>
2023-01-30 11:58:32 +0200

    routering + navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { combineLatest } from 'rxjs';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+{path:'home',component:HeaderComponent,title:'HomePage'},
+{path:'about',component:AboutComponent,title:'About me'},
+{path:'portfolio',component:PortfolioComponent,title:'Portfolio'},
+{path:'contact',component:ContactComponent,title:'Contact me'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 20 && window.innerWidth > 991) {
+    document.querySelector('nav')?.classList.add('navbar-shrink');
+  } else {
+    document.querySelector('nav')?.classList.remove('navbar-shrink');
+  }
+});
