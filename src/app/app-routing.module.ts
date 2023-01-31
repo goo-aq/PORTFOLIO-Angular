4df3fe4f7cfd389857097f290179469773d72708
goo-aq <120787130+goo-aq@users.noreply.github.com>
2023-01-31 18:28:36 +0200

    fix(routing not working)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ import { HeaderComponent } from './header/header.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HeaderComponent, title: 'HomePage' },
   { path: 'about', component: AboutComponent, title: 'About me' },
   { path: 'portfolio', component: PortfolioComponent, title: 'Portfolio' },
   { path: 'contact', component: ContactComponent, title: 'Contact me' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
 @NgModule({
